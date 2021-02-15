f2110bf10256a207a96d973648143878ee1c56eb
giftmischer69 <>
2021-02-15 01:23:49 +0100

    a new start :smiley:

--- dev_start_server.bat
+++
@@ -1,2 +0,0 @@
-start uvicorn server:app --reload --no-use-colors
-start chrome.exe http://127.0.0.1:8000/docs
--- view_server.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
-
-from cli import View, Prompt
-
-
